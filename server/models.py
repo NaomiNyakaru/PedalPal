2b38ccd2d5c2f1191ab46a93ea2142a8d3fda70f
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-11-13 10:42:25 +0300

    fix: user model

--- server/models.py
+++ server/models.py
@@ -25,12 +25,6 @@ class User(db.Model,SerializerMixin):
 
     serialize_rules = ('-ratings.user', '-rentals')
 
-    def set_password(self, password):
-        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
-
-    def check_password(self, password):
-        return bcrypt.check_password_hash(self.password, password)
-
 class Bike(db.Model,SerializerMixin):
     __tablename__ = 'bikes'
 
